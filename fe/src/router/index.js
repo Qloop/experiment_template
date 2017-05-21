19df14024816658ce233985cb6a1b7f8854d368b
sky-ADMIN <546485299@qq.com>
2017-05-21 17:32:49 +0800

    create router

--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Create from '../components/create/Create.vue'
+import Display from '../components/display/Display.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/create',
+      name: 'Create',
+      component: Create
+    },
+    {
+      path: '/display',
+      name: 'Display',
+      component: Display
+    },
+    {
+      path: '/report/:id',
+      name: 'Report',
+      component: Report
     }
   ]
 })
