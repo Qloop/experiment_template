0c7f103349c0532eff28c6a840c068d87f850d7d
sky-ADMIN <546485299@qq.com>
2017-05-21 19:52:12 +0800

    update 配置好vue-resource element-ui

--- fe/src/main.js
+++ fe/src/main.js
@@ -3,6 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+Vue.use(ElementUI)
+Vue.use(VueResource)
+
+Vue.http.options.emulateJSON = true
+Vue.http.options.credentials = true
 
 Vue.config.productionTip = false
 
