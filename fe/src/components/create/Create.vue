895eaaf82d2c4159529d32a185c3b3bf1623e1ee
sky-ADMIN <546485299@qq.com>
2017-05-22 16:30:17 +0800

    update

--- fe/src/components/create/Create.vue
+++ fe/src/components/create/Create.vue
@@ -4,27 +4,62 @@
     <div class="form-area">
       <div class="title">
         <h1>实验报告生成系统</h1>
-        <p>请在此处创建实验报告模板，请先仔细阅读要求。在文本中插入让学生填写的空请按如下方法插入，以" {{question0<span>}}"</span>的形式留空，必须是question打头，后跟一数字，从0开始数。例如：本次实验的目的是{{question0<span>}}</span>，通过{{question1<span>}}</span>方法，得到{{question3<span>}}结果。</span></p>
+        <p v-pre>请在此处创建实验报告模板，请先仔细阅读要求。在文本中插入让学生填写的空请按如下方法插入，以" {{question0}}"的形式留空，必须是question打头，后跟一数字，从0开始数。例如：本次实验的目的是{{question0}}，通过{{question1}}方法，得到{{question3}}结果。<br/>
+          仅有实验目的/实验原理/实验器材/实验要求/注意事项/实验数据记录和处理可以留下待学生填写的空，每部分的{{question}}都从0开始单独算。
+        </p>
+        <p class="link"><span v-on:click="toExample">样例</span></p>
       </div>
       <el-form ref="form" :model="form" label-width="80px">
         <el-form-item label="实验标题">
-          <el-input v-model="form.title"></el-input>
+          <el-input v-model="form.title" placeholder="输入实验标题"></el-input>
         </el-form-item>
-        <el-form-item label="实验目的">
-          <el-input type="textarea" v-model="form.goal"></el-input>
-        </el-form-item>
-        <el-form-item label="实验原理">
-          <el-input type="textarea" v-model="form.theory"></el-input>
-        </el-form-item>
-        <el-form-item label="实验器材">
-          <el-input type="textarea" v-model="form.equipment"></el-input>
-        </el-form-item>
-        <el-form-item label="实验要求">
-          <el-input type="textarea" v-model="form.demand"></el-input>
-        </el-form-item>
-        <el-form-item label="注意事项">
-          <el-input type="textarea" v-model="form.warning"></el-input>
-        </el-form-item>
+
+        <div class="form-group">
+          <el-form-item label="实验目的">
+            <el-input type="textarea" v-model="form.goal"></el-input>
+          </el-form-item>
+          <el-form-item label="问题数量">
+            <el-input v-model="form.goalSPCount"></el-input>
+          </el-form-item>
+        </div>
+
+        <div class="form-group">
+          <el-form-item label="实验原理">
+            <el-input type="textarea" v-model="form.theory"></el-input>
+          </el-form-item>
+          <el-form-item label="问题数量">
+            <el-input v-model="form.theorySPCount"></el-input>
+          </el-form-item>
+        </div>
+
+        <div class="form-group">
+          <el-form-item label="实验器材">
+            <el-input type="textarea" v-model="form.equipment"></el-input>
+          </el-form-item>
+          <el-form-item label="问题数量">
+            <el-input v-model="form.equipmentSPCount"></el-input>
+          </el-form-item>
+        </div>
+
+        <div class="form-group">
+          <el-form-item label="实验要求">
+            <el-input type="textarea" v-model="form.demand"></el-input>
+          </el-form-item>
+          <el-form-item label="问题数量">
+            <el-input v-model="form.demandSPCount"></el-input>
+          </el-form-item>
+        </div>
+
+        <div class="form-group">
+          <el-form-item label="注意事项">
+            <el-input type="textarea" v-model="form.warning"></el-input>
+          </el-form-item>
+          <el-form-item label="问题数量">
+            <el-input v-model="form.warning"></el-input>
+          </el-form-item>
+        </div>
+
+
         <el-form-item label="实验数据记录和处理">
           <el-input type="textarea" v-model="form.data"></el-input>
         </el-form-item>
@@ -65,6 +100,9 @@
             console.log(res);
           }
         )
+      },
+      toExample() {
+        this.$router.push('/example');
       }
     },
     components: {
@@ -85,6 +123,21 @@
         h1 {
           text-align: center;
         }
+
+        .link {
+          color: blue;
+          text-decoration: underline;
+
+          span {
+            cursor: pointer;
+          }
+        }
+      }
+
+      .form-group {
+        border: 2px solid blue;
+        padding: 20px 20px 0 0;
+        margin-top: 10px;
       }
     }
   }
