16e2e26a83fab17c4c798894d2e7c6af445bb4c1
sky-ADMIN <546485299@qq.com>
2017-05-23 22:45:56 +0800

    update style

--- fe/src/components/detail/Detail.vue
+++ fe/src/components/detail/Detail.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="detail-container">
-    <el-button type="primary" v-on:click="print">打印</el-button>
-    <h1>{{data.title}}</h1>
-    <h2>实验目的</h2>
-    <p>{{data.goalRept}}</p>
-    <h2>实验原理</h2>
-    <p>{{data.theoryRept}}</p>
-    <h2>实验器材</h2>
-    <p>{{data.equipment}}</p>
-    <h2>实验要求</h2>
-    <p>{{data.demand}}</p>
-    <h2>注意事项</h2>
-    <p>{{data.warning}}</p>
-    <h2>实验数据记录与处理</h2>
-    <p>{{data.data}}</p>
-    <h2>思考与讨论</h2>
-    <p>{{data.thinking}}</p>
+    <el-button class="hidden-print" type="primary" v-on:click="print">打印</el-button>
+    <div class="header">
+      <h1>123{{data.title}}</h1>
+      <div class="student-msg">
+        <h2>班级：{{data.stuClass}}</h2>
+        <h2>姓名：{{data.stuName}}</h2>
+        <h2>学号：{{data.stuId}}</h2>
+      </div>
+    </div>
+    <div class="box">
+      <h2 class="page-next">实验目的</h2>
+      <p>{{data.goalRept}}</p>
+    </div>
+    <div class="box">
+      <h2>实验原理</h2>
+      <p>{{data.theoryRept}}</p>
+    </div>
+    <div class="box">
+      <h2>实验器材</h2>
+      <p>{{data.equipment}}</p>
+    </div>
+    <div class="box">
+      <h2>实验要求</h2>
+      <p>{{data.demand}}</p>
+    </div>
+    <div class="box">
+      <h2>注意事项</h2>
+      <p>{{data.warning}}</p>
+    </div>
+    <div class="box">
+      <h2>实验数据记录与处理</h2>
+      <p>{{data.data}}</p>
+    </div>
+    <div class="box">
+      <h2>思考与讨论</h2>
+      <p>{{data.thinking}}</p>
+    </div>
   </div>
 </template>
 
@@ -47,7 +68,40 @@
 </script>
 
 <style lang="less">
+
+  @media print {
+    .hidden-print {
+      display: none !important;
+    }
+  }
+
   .detail-container {
+    margin: 15px 20px;
+
+    .header {
+      text-align: center;
+      margin-top: 350px;
+
+      .student-msg {
+        margin-top: 30px;
+        padding: 0 10%;
+        margin-left: 150px;
+        text-align: left;
+        line-height: 40px;
+      }
+    }
+
+    .box {
+
+      p {
+        border: 1px solid black;
+        padding: 10px;
+      }
+    }
+
+    .page-next {
+      page-break-before: always;
+    }
 
   }
 </style>
