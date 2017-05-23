49faed8269cb769a5dc750cc22c884f73f190f19
sky-ADMIN <546485299@qq.com>
2017-05-23 21:15:04 +0800

    update

---
+++ fe/src/components/reportlist/ReportList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="report-list-container">
+    <top-nav activeIndex="0"></top-nav>
+    <div class="table-container">
+      <el-table
+        :data="reportList"
+        border
+        style="width: 100%">
+        <el-table-column
+          prop="stuClass"
+          label="班级">
+        </el-table-column>
+
+        <el-table-column
+          prop="stuId"
+          label="学号">
+        </el-table-column>
+
+        <el-table-column
+          prop="stuName"
+          label="姓名">
+        </el-table-column>
+
+        <el-table-column
+          label="操作"
+          width="100">
+          <template scope="scope">
+            <el-button v-on:click="handleTableClick(scope)" type="text" size="small">查看</el-button>
+            <!--<el-button type="text" size="small">编辑</el-button>-->
+          </template>
+        </el-table-column>
+
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+  import API from '../../config/request';
+  import TopNav from '../common/TopNav.vue';
+  export default {
+    data() {
+      return {
+        reportList: []
+      }
+    },
+    created() {
+      this.loadData();
+    },
+    methods: {
+      loadData() {
+        this.$http.get(API.reportList(this.$route.params.id)).then(
+          (res) => {
+            console.log(res.data)
+            this.reportList = res.data.reportBeanList;
+          }
+        )
+      }
+    },
+    components: {
+      TopNav
+    }
+  }
+</script>
+
+<style lang="less">
+  .report-list-container {
+
+    .table-container {
+      margin: 20px 15%;
+    }
+  }
+
+</style>
